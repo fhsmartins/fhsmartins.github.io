fa011a70ac51955bb9eefbf67d6770c1977f169f
Fabio Martins <fhsm13@gmail.com>
2021-05-26 16:49:59 -0300

    Contacts

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Fabio Martins'"; //letter subject
+	$to = 'fhsm13@outlook.com'; // Enter Your E-mail
+	$from = 'fhsmartins@fastmail.de'; // Admin e-mail
+	$fromName = 'Fabio Martins'; // Your company name
+	$charset = 'UTF-8';
+?>
